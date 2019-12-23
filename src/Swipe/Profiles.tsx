19d0aa91e641509843ca5b3551ca9e21f1e70a51
William Candillon <wcandillon@gmail.com>
2019-12-23 16:48:25 +0100

    🔧

--- src/Swipe/Profiles.tsx
+++ src/Swipe/Profiles.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Dimensions, SafeAreaView, StyleSheet, View } from "react-native";
+import { View, Dimensions, SafeAreaView, StyleSheet } from "react-native";
 import Animated from "react-native-reanimated";
 import { Feather as Icon } from "@expo/vector-icons";
 
@@ -7,22 +7,8 @@ import { useMemoOne } from "use-memo-one";
 import { RectButton } from "react-native-gesture-handler";
 import Card, { Profile } from "./Profile";
 import Swipeable from "./Swipeable";
-import { StyleGuide, timing } from "../components";
+import { timing, StyleGuide } from "../components";
 
-const {
-  Value,
-  Clock,
-  interpolate,
-  concat,
-  Extrapolate,
-  block,
-  cond,
-  set,
-  useCode,
-  not,
-  clockRunning,
-  call
-} = Animated;
 const { width, height } = Dimensions.get("window");
 const deltaX = width / 2;
 const α = Math.PI / 12;
@@ -71,73 +57,8 @@ interface ProfilesProps {
 
 export default ({ profiles }: ProfilesProps) => {
   const [index, setIndex] = useState(0);
-  const { x, y, offsetX, like, dislike } = useMemoOne(
-    () => ({
-      x: new Value(0),
-      y: new Value(0),
-      offsetX: new Value(0),
-      like: new Value(0) as Animated.Value<0 | 1>,
-      dislike: new Value(0) as Animated.Value<0 | 1>
-    }),
-    []
-  );
-  const onSnap = useMemoOne(
-    () => ([point]: readonly number[]) => {
-      if (point !== 0) {
-        offsetX.setValue(0);
-        setIndex((index + 1) % profiles.length);
-      }
-    },
-    [index]
-  );
   const profile = profiles[index];
-  const rotateZ = concat(
-    interpolate(x, {
-      inputRange: [-1 * deltaX, deltaX],
-      outputRange: [α, -1 * α],
-      extrapolate: Extrapolate.CLAMP
-    }),
-    "rad"
-  );
-  const likeOpacity = interpolate(x, {
-    inputRange: [0, deltaX / 4],
-    outputRange: [0, 1]
-  });
-  const nopeOpacity = interpolate(x, {
-    inputRange: [(-1 * deltaX) / 4, 0],
-    outputRange: [1, 0]
-  });
-  const translateX = x;
-  const translateY = y;
-  const clock = new Clock();
-  useCode(
-    () =>
-      block([
-        cond(dislike, [
-          set(
-            offsetX,
-            timing({
-              clock,
-              from: offsetX,
-              to: -A
-            })
-          ),
-          cond(not(clockRunning(clock)), [set(dislike, 0), call([], onSnap)])
-        ]),
-        cond(like, [
-          set(
-            offsetX,
-            timing({
-              clock,
-              from: offsetX,
-              to: A
-            })
-          ),
-          cond(not(clockRunning(clock)), [set(like, 0), call([], onSnap)])
-        ])
-      ]),
-    [onSnap, like, dislike]
-  );
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -147,19 +68,23 @@ export default ({ profiles }: ProfilesProps) => {
       <View style={styles.cards}>
         <Animated.View
           style={{
-            ...StyleSheet.absoluteFillObject,
-            transform: [{ translateX }, { translateY }, { rotateZ }]
+            ...StyleSheet.absoluteFillObject
           }}
         >
-          <Card {...{ profile, likeOpacity, nopeOpacity }} />
+          <Card {...{ profile }} />
         </Animated.View>
-        <Swipeable key={index} {...{ snapPoints, onSnap, x, y, offsetX }} />
       </View>
       <View style={styles.footer}>
-        <RectButton style={styles.circle} onPress={() => dislike.setValue(1)}>
+        <RectButton
+          style={styles.circle}
+          onPress={() => setIndex((index + 1) % profiles.length)}
+        >
           <Icon name="x" size={32} color="#ec5288" />
         </RectButton>
-        <RectButton style={styles.circle} onPress={() => like.setValue(1)}>
+        <RectButton
+          style={styles.circle}
+          onPress={() => setIndex((index + 1) % profiles.length)}
+        >
           <Icon name="heart" size={32} color="#6ee3b4" />
         </RectButton>
       </View>
--- src/Swipe/Swipeable.tsx
+++ src/Swipe/Swipeable.tsx
@@ -1,68 +1,7 @@
 import * as React from "react";
-import { StyleSheet } from "react-native";
-import { onGestureEvent } from "react-native-redash";
-import Animated from "react-native-reanimated";
-import { PanGestureHandler, State } from "react-native-gesture-handler";
-import { withSpring } from "../components";
 
-const { Value, useCode, block, set } = Animated;
-const config = {
-  damping: 40,
-  mass: 1,
-  stiffness: 300,
-  overshootClamping: false,
-  restSpeedThreshold: 1,
-  restDisplacementThreshold: 1
-};
-interface SwipeableProps {
-  x: Animated.Value<number>;
-  y: Animated.Value<number>;
-  offsetX?: Animated.Value<number>;
-  offsetY?: Animated.Value<number>;
-  snapPoints: number[];
-  onSnap: (value: readonly number[]) => void;
-}
+interface SwipeableProps {}
 
-export default ({
-  x,
-  y,
-  offsetX,
-  offsetY,
-  snapPoints,
-  onSnap
-}: SwipeableProps) => {
-  const translationX = new Value(0);
-  const translationY = new Value(0);
-  const velocityX = new Value(0);
-  const velocityY = new Value(0);
-  const state = new Value(State.UNDETERMINED);
-  const gestureHandler = onGestureEvent({
-    velocityX,
-    translationX,
-    translationY,
-    state
-  });
-  const translateX = withSpring({
-    value: translationX,
-    velocity: velocityX,
-    offset: offsetX,
-    state,
-    snapPoints,
-    onSnap,
-    config
-  });
-  const translateY = withSpring({
-    value: translationY,
-    velocity: velocityY,
-    offset: offsetY || new Value(0),
-    state,
-    snapPoints: [0],
-    config
-  });
-  useCode(() => block([set(x, translateX), set(y, translateY)]), []);
-  return (
-    <PanGestureHandler {...gestureHandler}>
-      <Animated.View style={StyleSheet.absoluteFill} />
-    </PanGestureHandler>
-  );
+export default ({  }: SwipeableProps) => {
+  return null;
 };
