cc981a3fd00bc78b16544fded65cebf6cb8b20aa
William Candillon <wcandillon@gmail.com>
2019-12-23 16:30:17 +0100

    🔧

--- src/Svg/Svg.tsx
+++ src/Svg/Svg.tsx
@@ -3,14 +3,9 @@ import { Dimensions, StyleSheet, View } from "react-native";
 import Svg, { Ellipse } from "react-native-svg";
 import Constants from "expo-constants";
 
-import { PanGestureHandler, State } from "react-native-gesture-handler";
-import Animated from "react-native-reanimated";
-import { onGestureEvent } from "react-native-redash";
-import { StyleGuide, withTransition } from "../components";
+import { StyleGuide } from "../components";
 
-const { Value, sub, multiply, eq, cond, max, abs } = Animated;
 const { width, height } = Dimensions.get("window");
-const AnimatedEllipse = Animated.createAnimatedComponent(Ellipse);
 const containerWidth = width;
 const containerHeight = height - Constants.statusBarHeight - 44;
 const center = {
@@ -24,50 +19,18 @@ const styles = StyleSheet.create({
   }
 });
 
-const canvas2Euclidean = (
-  x: Animated.Node<number>,
-  y: Animated.Node<number>
-) => {
-  return {
-    rx: sub(x, center.x),
-    ry: multiply(sub(y, center.y), -1)
-  };
-};
-
 export default () => {
-  const x = new Value(0);
-  const y = new Value(0);
-  const velocityX = new Value(0);
-  const velocityY = new Value(0);
-  const state = new Value(State.UNDETERMINED);
-  const gestureHandler = onGestureEvent({
-    x,
-    y,
-    velocityX,
-    velocityY,
-    state
-  });
-  const isActive = eq(state, State.ACTIVE);
-  const targetX = cond(isActive, x, center.x);
-  const targetY = cond(isActive, y, center.y);
-  const { rx, ry } = canvas2Euclidean(
-    withTransition(targetX, velocityX),
-    withTransition(targetY, velocityY)
-  );
   return (
     <View style={styles.container}>
       <Svg style={StyleSheet.absoluteFill}>
-        <AnimatedEllipse
+        <Ellipse
           cx={center.x}
           cy={center.y}
-          rx={max(abs(rx), radius)}
-          ry={max(abs(ry), radius)}
+          rx={radius}
+          ry={radius}
           fill={StyleGuide.palette.primary}
         />
       </Svg>
-      <PanGestureHandler {...gestureHandler}>
-        <Animated.View style={StyleSheet.absoluteFill} />
-      </PanGestureHandler>
     </View>
   );
 };
