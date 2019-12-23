2bb34c3acb02202e1151fa5c845bd49a8643c790
William Candillon <wcandillon@gmail.com>
2019-12-23 16:57:56 +0100

    🔧

--- src/PanGesture/PanGesture.tsx
+++ src/PanGesture/PanGesture.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Dimensions, Platform, StyleSheet, View } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
 import { PanGestureHandler, State } from "react-native-gesture-handler";
 import Animated from "react-native-reanimated";
 import Constants from "expo-constants";
@@ -11,10 +11,7 @@ import { CARD_HEIGHT, CARD_WIDTH } from "../components/Card";
 const { Value, diffClamp, cond, set, eq, add } = Animated;
 const { width, height } = Dimensions.get("window");
 const containerWidth = width;
-const containerHeight =
-  height - Constants.statusBarHeight - (Platform.OS === "ios" ? 44 : 52);
-const offsetX = new Value((containerWidth - CARD_WIDTH) / 2);
-const offsetY = new Value((containerHeight - CARD_HEIGHT) / 2);
+const containerHeight = height - Constants.statusBarHeight - 44;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -23,51 +20,12 @@ const styles = StyleSheet.create({
 });
 const [card] = cards;
 
-const withOffset = (
-  value: Animated.Node<number>,
-  state: Animated.Value<State>,
-  offset: Animated.Value<number> = new Value(0)
-) =>
-  cond(
-    eq(state, State.END),
-    [set(offset, add(offset, value)), offset],
-    add(offset, value)
-  );
-
 export default () => {
-  const state = new Value(State.UNDETERMINED);
-  const translationX = new Value(0);
-  const translationY = new Value(0);
-  const velocityX = new Value(0);
-  const velocityY = new Value(0);
-  const gestureHandler = onGestureEvent({
-    state,
-    translationX,
-    translationY,
-    velocityX,
-    velocityY
-  });
-  const translateX = diffClamp(
-    withOffset(translationX, state, offsetX),
-    0,
-    containerWidth - CARD_WIDTH
-  );
-  const translateY = diffClamp(
-    withOffset(translationY, state, offsetY),
-    0,
-    containerHeight - CARD_HEIGHT
-  );
   return (
     <View style={styles.container}>
-      <PanGestureHandler {...gestureHandler}>
-        <Animated.View
-          style={{
-            transform: [{ translateX }, { translateY }]
-          }}
-        >
-          <Card {...{ card }} />
-        </Animated.View>
-      </PanGestureHandler>
+      <View>
+        <Card {...{ card }} />
+      </View>
     </View>
   );
 };
