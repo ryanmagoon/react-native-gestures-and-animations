4d5053da09668883857a90fe2963faddb956e5bb
William Candillon <wcandillon@gmail.com>
2019-12-23 16:53:14 +0100

    🔧

--- src/Decay/Decay.tsx
+++ src/Decay/Decay.tsx
@@ -8,23 +8,7 @@ import { onGestureEvent } from "react-native-redash";
 import { Card, StyleGuide, cards } from "../components";
 import { CARD_HEIGHT, CARD_WIDTH } from "../components/Card";
 
-const {
-  Clock,
-  Value,
-  diffClamp,
-  cond,
-  set,
-  eq,
-  add,
-  decay: reDecay,
-  clockRunning,
-  startClock,
-  stopClock,
-  block,
-  and,
-  not,
-  neq
-} = Animated;
+const { Value, diffClamp, cond, set, eq, add } = Animated;
 const { width, height } = Dimensions.get("window");
 const containerWidth = width;
 const containerHeight = height - Constants.statusBarHeight - 44;
@@ -38,80 +22,34 @@ const styles = StyleSheet.create({
 });
 const [card] = cards;
 
-interface WithDecayProps {
-  value: Animated.Adaptable<number>;
-  velocity: Animated.Adaptable<number>;
-  state: Animated.Value<State>;
-  offset?: Animated.Value<number>;
-  deceleration?: number;
-}
-
-const withDecay = (config: WithDecayProps) => {
-  const { value, velocity, state, offset, deceleration } = {
-    offset: new Value(0),
-    deceleration: 0.998,
-    ...config
-  };
-  const clock = new Clock();
-  const decayState = {
-    finished: new Value(0),
-    velocity: new Value(0),
-    position: new Value(0),
-    time: new Value(0)
-  };
-
-  const isDecayInterrupted = and(eq(state, State.BEGAN), clockRunning(clock));
-  const finishDecay = [set(offset, decayState.position), stopClock(clock)];
-
-  return block([
-    cond(isDecayInterrupted, finishDecay),
-    cond(neq(state, State.END), [
-      set(decayState.finished, 0),
-      set(decayState.position, add(offset, value))
-    ]),
-    cond(eq(state, State.END), [
-      cond(and(not(clockRunning(clock)), not(decayState.finished)), [
-        set(decayState.velocity, velocity),
-        set(decayState.time, 0),
-        startClock(clock)
-      ]),
-      reDecay(clock, decayState, { deceleration }),
-      cond(decayState.finished, finishDecay)
-    ]),
-    decayState.position
-  ]);
-};
+// TODO: replace with withOffset from redash
+const withOffset = (
+  value: Animated.Node<number>,
+  state: Animated.Value<State>,
+  offset: Animated.Value<number> = new Value(0)
+) =>
+  cond(
+    eq(state, State.END),
+    [set(offset, add(offset, value)), offset],
+    add(offset, value)
+  );
 
 export default () => {
   const state = new Value(State.UNDETERMINED);
   const translationX = new Value(0);
   const translationY = new Value(0);
-  const velocityX = new Value(0);
-  const velocityY = new Value(0);
   const gestureHandler = onGestureEvent({
     state,
     translationX,
-    translationY,
-    velocityX,
-    velocityY
+    translationY
   });
   const translateX = diffClamp(
-    withDecay({
-      value: translationX,
-      velocity: velocityX,
-      state,
-      offset: offsetX
-    }),
+    withOffset(translationX, state, offsetX),
     0,
     containerWidth - CARD_WIDTH
   );
   const translateY = diffClamp(
-    withDecay({
-      value: translationY,
-      velocity: velocityY,
-      state,
-      offset: offsetY
-    }),
+    withOffset(translationY, state, offsetY),
     0,
     containerHeight - CARD_HEIGHT
   );
