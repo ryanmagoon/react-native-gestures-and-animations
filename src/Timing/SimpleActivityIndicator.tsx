828f07a0d9cb7809345499418fcede1fd1067a21
Ryan Magoon <mr.magoon5@gmail.com>
2020-01-23 23:29:56 -0500

    dots timing transition working one way

--- src/Timing/SimpleActivityIndicator.tsx
+++ src/Timing/SimpleActivityIndicator.tsx
@@ -1,25 +1,25 @@
-import * as React from "react";
-import { Dimensions, StyleSheet, View } from "react-native";
-import Animated from "react-native-reanimated";
-import { StyleGuide } from "../components";
+import * as React from 'react'
+import { Dimensions, StyleSheet, View } from 'react-native'
+import Animated from 'react-native-reanimated'
+import { StyleGuide } from '../components'
 
-const { width: wWidth } = Dimensions.get("window");
-const width = wWidth * 0.8;
-const { interpolate, Extrapolate } = Animated;
-const size = 32;
+const { width: wWidth } = Dimensions.get('window')
+const width = wWidth * 0.8
+const { interpolate, Extrapolate } = Animated
+const size = 32
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   container: {
     height: width,
     width,
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    backgroundColor: "#d3d3d3",
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    backgroundColor: '#d3d3d3',
     borderTopLeftRadius: width / 2,
     borderTopRightRadius: width / 2,
     borderBottomLeftRadius: width / 2
@@ -30,19 +30,34 @@ const styles = StyleSheet.create({
     borderRadius: size / 2,
     backgroundColor: StyleGuide.palette.primary
   }
-});
+})
 
-interface SimpleActivityIndicatorProps {}
+interface SimpleActivityIndicatorProps {
+  progress: Animated.Value<number>
+}
 
-export default () => {
-  const bubbles = [0, 1, 2];
+export default ({ progress }) => {
+  const bubbles = [0, 1, 2]
+  const delta = 1 / bubbles.length
   return (
     <View style={styles.root}>
       <View style={styles.container}>
         {bubbles.map(i => {
-          return <View key={i} style={styles.bubble} />;
+          const start = i * delta
+          const end = start + delta
+          const scale = interpolate(progress, {
+            inputRange: [start, end],
+            outputRange: [1, 1.5],
+            extrapolate: Extrapolate.CLAMP
+          })
+          return (
+            <Animated.View
+              key={i}
+              style={[styles.bubble, { transform: [{ scale }] }]}
+            />
+          )
         })}
       </View>
     </View>
-  );
-};
+  )
+}
--- src/Timing/Timing.tsx
+++ src/Timing/Timing.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
-import Animated, { Easing } from "react-native-reanimated";
-import { loop } from "react-native-redash";
-import { useMemoOne } from "use-memo-one";
+import React, { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import Animated, { Easing } from 'react-native-reanimated'
+import { loop } from 'react-native-redash'
+import { useMemoOne } from 'use-memo-one'
 
-import SimpleActivityIndicator from "./SimpleActivityIndicator";
-import { Button, StyleGuide } from "../components";
+import SimpleActivityIndicator from './SimpleActivityIndicator'
+import { Button, StyleGuide } from '../components'
 
 const {
   Clock,
@@ -18,21 +18,46 @@ const {
   stopClock,
   clockRunning,
   and,
-  not
-} = Animated;
+  not,
+  timing
+} = Animated
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "space-between",
+    justifyContent: 'space-between',
     backgroundColor: StyleGuide.palette.background
   }
-});
+})
+
+const runTiming = (clock: Animated.Clock) => {
+  const state = {
+    finished: new Value(0),
+    position: new Value(0),
+    frameTime: new Value(0),
+    time: new Value(0)
+  }
+  const config = {
+    toValue: new Value(1),
+    duration: 1000,
+    easing: Easing.linear
+  }
+  return block([
+    cond(not(clockRunning(clock)), startClock(clock)),
+    timing(clock, state, config),
+    state.position
+  ])
+}
 
 export default () => {
+  const progress = new Value(0)
+  const clock = new Clock()
+
+  useCode(block([set(progress, runTiming(clock))]), [])
+
   return (
     <View style={styles.container}>
-      <SimpleActivityIndicator />
+      <SimpleActivityIndicator {...{ progress }} />
     </View>
-  );
-};
+  )
+}
